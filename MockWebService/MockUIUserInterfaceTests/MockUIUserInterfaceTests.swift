5eb5e62a4d660978ad144030425f96cd28e0bcda
Jason <sanchezjason@me.com>
2024-09-14 14:23:19 -0600

    mock_web_service/login/- UI test added

--- MockWebService/MockUIUserInterfaceTests/MockUIUserInterfaceTests.swift
+++ MockWebService/MockUIUserInterfaceTests/MockUIUserInterfaceTests.swift
@@ -1,23 +1,22 @@
 //
-//  MockWebServiceInterfaceTestsLaunchTests.swift
-//  MockWebServiceInterfaceTests
+//  MockUIUserInterfaceTests.swift
+//  MockUIUserInterfaceTests
 //
 //  Created by Jason Sanchez on 9/14/24.
 //
 
 import XCTest
 
-class MockWebServiceInterfaceTestsLaunchTests: XCTestCase {
+class when_user_taps_on_login_button: XCTestCase {
 
     func test_should_display_error_message_for_missing_required_fields() {
-        
         let app = XCUIApplication()
         continueAfterFailure = false
         app.launch()
         
-        let usernameTextField = app.textFields["usernameTextField"]
-        usernameTextField.tap()
-        usernameTextField.typeText("")
+        let userNameTextField = app.textFields["usernameTextField"]
+        userNameTextField.tap()
+        userNameTextField.typeText("")
         
         let passwordTextField = app.textFields["passwordTextField"]
         passwordTextField.tap()
@@ -29,6 +28,5 @@ class MockWebServiceInterfaceTestsLaunchTests: XCTestCase {
         let messageText = app.staticTexts["messageText"]
         
         XCTAssertEqual(messageText.label, "Required fields are missing")
-        
     }
 }
