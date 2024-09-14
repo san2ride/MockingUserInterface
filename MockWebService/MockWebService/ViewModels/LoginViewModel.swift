89d2d2ee6c88cf9c3615788724e61f7303037480
Jason <sanchezjason@me.com>
2024-09-14 13:41:15 -0600

    mock_web_service/login/- LoginViewModel

---
+++ MockWebService/MockWebService/ViewModels/LoginViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  LoginViewModel.swift
+//  MockWebService
+//
+//  Created by Jason Sanchez on 9/14/24.
+//
+
+import Foundation
+
+enum LoginStatus {
+    case none
+    case authenticated
+    case denied
+    case validationFailed
+}
+
+class LoginViewModel: ObservableObject {
+    
+    @Published var username: String = ""
+    @Published var password: String = ""
+    @Published var loginStatus: LoginStatus = .none
+    
+    private var service: Webservice
+    
+    init(service: Webservice) {
+        self.service = service
+    }
+    
+    func login() {
+        if username.isEmpty || password.isEmpty {
+            self.loginStatus = .validationFailed
+            return
+        }
+        service.login(username: username, password: password) { result in
+            switch result {
+                case .success():
+                        DispatchQueue.main.async {
+                            self.loginStatus = .authenticated
+                        }
+                case .failure(_):
+                    DispatchQueue.main.async {
+                        self.loginStatus = .denied
+                    }
+            }
+        }
+    }
+}
