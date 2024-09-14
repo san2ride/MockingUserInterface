dbfede1a8ef071909da6c8d85f74790596a49cbe
Jason <sanchezjason@me.com>
2024-09-14 13:39:09 -0600

    mock_web_service/login/- LoginView

--- MockWebService/MockWebService/MockWebServiceApp.swift
+++ MockWebService/MockWebService/MockWebServiceApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MockWebServiceApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
         }
     }
 }
---
+++ MockWebService/MockWebService/Views/LoginView.swift
@@ -0,0 +1,58 @@
+//
+//  LoginView.swift
+//  MockWebService
+//
+//  Created by Jason Sanchez on 9/14/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    var errorMessage: String {
+        switch loginVM.loginStatus {
+            case .denied:
+                return "Invalid credentials"
+            case .validationFailed:
+                return "Required fields are missing"
+            default:
+                return ""
+        }
+    }
+    
+    var body: some View {
+        let _ = Self._printChanges()
+        NavigationView {
+            VStack {
+                Form {
+                    TextField("User name", text: $loginVM.username)
+                        .accessibilityIdentifier("usernameTextField")
+                    TextField("Password", text: $loginVM.password)
+                        .accessibilityIdentifier("passwordTextField")
+                    HStack {
+                        Spacer()
+                        Button {
+                            loginVM.login()
+                        } label: {
+                            Text("Login")
+                                .accessibilityIdentifier("loginButton")
+                        }
+                        Spacer()
+                    }
+                    Text(errorMessage)
+                        .accessibilityIdentifier("messageText")
+                }
+                NavigationLink(isActive: .constant(loginVM.loginStatus == .authenticated)) {
+                    DashboardView()
+                } label: {
+                    EmptyView()
+                }
+            }
+            .navigationTitle("Login")
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
